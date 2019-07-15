5a6ffe5f42bdfce84005ca899787bdf124cc2f22
Gihwan Kim <ghkim3221@gmail.com>
2019-07-15 20:34:03 +0900

    프로그램 시작 시 외부 설정 파일에서 설정 로드

---
+++ src/main/java/databute/databuter/DatabuterConfiguration.java
@@ -0,0 +1,12 @@
+package databute.databuter;
+
+import com.google.common.base.MoreObjects;
+
+public class DatabuterConfiguration {
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .toString();
+    }
+}
---
+++ src/main/java/databute/databuter/DatabuterConstants.java
@@ -0,0 +1,10 @@
+package databute.databuter;
+
+public final class DatabuterConstants {
+
+    public static final String CONFIGURATION_PATH = System.getProperty("databuter.configuration", "config.yaml");
+
+    private DatabuterConstants() {
+
+    }
+}
