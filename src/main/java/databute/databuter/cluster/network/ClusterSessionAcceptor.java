b7fc38b7749a2fcd42b9566ac7f2c660fbdb436f
Gihwan Kim <ghkim3221@gmail.com>
2019-07-16 21:23:05 +0900

    바이트 버퍼 - 패킷 변환 코덱 구현

--- src/main/java/databute/databuter/cluster/network/ClusterSessionAcceptor.java
+++ src/main/java/databute/databuter/cluster/network/ClusterSessionAcceptor.java
@@ -1,6 +1,8 @@
 package databute.databuter.cluster.network;
 
 import databute.databuter.network.AbstractSessionAcceptor;
+import databute.databuter.network.packet.codec.ByteToPacketDecoder;
+import databute.databuter.network.packet.codec.PacketToByteEncoder;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
@@ -19,6 +21,9 @@ public class ClusterSessionAcceptor extends AbstractSessionAcceptor {
             @Override
             protected void initChannel(SocketChannel channel) {
                 final ChannelPipeline pipeline = channel.pipeline();
+
+                pipeline.addLast(new PacketToByteEncoder());
+                pipeline.addLast(new ByteToPacketDecoder());
 
                 pipeline.addLast(new InboundClusterChannelHandler());
             }
