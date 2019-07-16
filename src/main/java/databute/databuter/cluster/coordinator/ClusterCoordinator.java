d6ff2151a1def88b560095e414c582682005085d
Gihwan Kim <ghkim3221@gmail.com>
2019-07-16 17:38:53 +0900

    ZooKeeper 디렉토리에 이벤트 리스너 등록

--- src/main/java/databute/databuter/cluster/coordinator/ClusterCoordinator.java
+++ src/main/java/databute/databuter/cluster/coordinator/ClusterCoordinator.java
@@ -3,7 +3,11 @@ package databute.databuter.cluster.coordinator;
 import databute.databuter.cluster.ClusterException;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.ZKPaths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +16,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class ClusterCoordinator {
 
     private static final Logger logger = LoggerFactory.getLogger(ClusterCoordinator.class);
+
+    private PathChildrenCache cache;
 
     private final ClusterCoordinatorConfiguration configuration;
 
@@ -27,12 +33,50 @@ public class ClusterCoordinator {
 
     public void connect() throws ClusterException {
         try {
-            logger.debug("Connecting to the ZooKeeper...");
-            curator.start();
-            curator.blockUntilConnected();
-            logger.debug("Connected with the ZooKeeper.");
+            connectToZooKeeper();
         } catch (InterruptedException e) {
             throw new ClusterException("Failed to connect to the ZooKeeper.", e);
         }
+        try {
+            registerCacheEventListener();
+        } catch (Exception e) {
+            throw new ClusterException("Failed to register cache event listener.", e);
+        }
+    }
+
+    private void connectToZooKeeper() throws InterruptedException {
+        logger.debug("Connecting to the ZooKeeper...");
+        curator.start();
+        curator.blockUntilConnected();
+        logger.debug("Connected with the ZooKeeper.");
+    }
+
+    private void registerCacheEventListener() throws Exception {
+        final String path = ZKPaths.makePath(configuration.path(), "discovery");
+
+        cache = new PathChildrenCache(curator, path, true);
+        cache.getListenable().addListener(this::onCacheEvent);
+        cache.start(PathChildrenCache.StartMode.POST_INITIALIZED_EVENT);
+        logger.debug("Registered cache event listener at {}", path);
+    }
+
+    @SuppressWarnings("unused")
+    private void onCacheEvent(CuratorFramework curator, PathChildrenCacheEvent event) {
+        switch (event.getType()) {
+            case INITIALIZED:
+                onInitialized();
+                break;
+            case CHILD_ADDED:
+                onAdded(event.getData());
+                break;
+        }
+    }
+
+    private void onInitialized() {
+
+    }
+
+    private void onAdded(ChildData data) {
+        checkNotNull(data, "data");
     }
 }
