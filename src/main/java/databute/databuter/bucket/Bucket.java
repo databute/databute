199f3ae30db9e2c07c9c17d68de4061152ad5113
Gihwan Kim <ghkim3221@gmail.com>
2019-07-26 15:36:43 +0900

    버킷 클래스를 추상 클래스로 변경

--- src/main/java/databute/databuter/bucket/Bucket.java
+++ src/main/java/databute/databuter/bucket/Bucket.java
@@ -7,16 +7,16 @@ import java.util.UUID;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-public class Bucket {
+public abstract class Bucket {
 
     private final String id;
     private final BucketConfiguration configuration;
 
-    public Bucket() {
+    protected Bucket() {
         this(new BucketConfiguration(UUID.randomUUID().toString(), Databuter.instance().id()));
     }
 
-    public Bucket(BucketConfiguration configuration) {
+    protected Bucket(BucketConfiguration configuration) {
         checkNotNull(configuration, "configuration");
         this.id = configuration.id();
         this.configuration = configuration;
