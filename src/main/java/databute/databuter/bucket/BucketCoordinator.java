bcac982832a7982408bf9e20eabbe91b33ee447d
Gihwan Kim <ghkim3221@gmail.com>
2019-07-26 22:13:27 +0900

    버킷 생성 및 업데이트 로직 재작성 (#71)

--- src/main/java/databute/databuter/bucket/BucketCoordinator.java
+++ src/main/java/databute/databuter/bucket/BucketCoordinator.java
@@ -74,112 +74,111 @@ public class BucketCoordinator {
     }
 
     private void onInitialized() {
-        makeStandbyIfStanbyNodeIsEmpty();
-
-        try {
-            makeBucket();
-        } catch (BucketException e) {
-            logger.error("Failed making Bucket.", e);
-        }
-    }
-
-    private void makeStandbyIfStanbyNodeIsEmpty() {
-        for (Bucket bucket : bucketGroup) {
-            if (availableBucketCount.get() <= 0) {
-                break;
-            }
-
-            if (StringUtils.isEmpty(bucket.standbyNodeId())) {
-                try {
-                    final String nodeId = Databuter.instance().id();
-                    bucket.configuration().standbyNodeId(nodeId);
-
-                    updateBucket(bucket);
-
-                    availableBucketCount.getAndDecrement();
-
-                    logger.debug("Bucket {} is set as standby.", bucket.id());
-                } catch (Exception e) {
-                    logger.error("Failed to make bucket {} as standby.", bucket.id(), e);
-                }
-            }
-        }
-    }
-
-    private void updateBucket(Bucket bucket) throws Exception {
-        final String json = new Gson().toJson(bucket.configuration());
-        final String path = ZKPaths.makePath(zooKeeperConfiguration.path(), "bucket", bucket.id());
-
-        Databuter.instance().curator()
-                .setData()
-                .forPath(path, json.getBytes());
-        logger.debug("Bucket {} is updated.", bucket.id());
-    }
-
-    private void makeBucket() throws BucketException {
-        logger.debug("Making {} bucket...", availableBucketCount.get());
-
         while (availableBucketCount.get() > 0) {
-            final String nodeId = Databuter.instance().id();
-            final BucketConfiguration bucketConfiguration = new BucketConfiguration().activeNodeId(nodeId);
-            final Bucket bucket = new LocalBucket(bucketConfiguration);
-            final boolean added = bucketGroup.add(bucket);
-            if (!added) {
-                throw new BucketException("Found duplcated bucket " + bucket);
-            }
-
             try {
-                registerBucket(bucket);
+                final String nodeId = Databuter.instance().id();
+                final BucketConfiguration bucketConfiguration = new BucketConfiguration().activeNodeId(nodeId);
+                final Bucket localBucket = new LocalBucket(bucketConfiguration);
+
+                final String json = new Gson().toJson(localBucket.configuration());
+                final String path = ZKPaths.makePath(zooKeeperConfiguration.path(), "bucket", localBucket.id());
+                final String createdPath = Databuter.instance().curator()
+                        .create()
+                        .creatingParentContainersIfNeeded()
+                        .withMode(CreateMode.PERSISTENT)
+                        .forPath(path, json.getBytes());
+
+                final boolean added = bucketGroup.add(localBucket);
+                if (added) {
+                    final long remainingBucketCount = availableBucketCount.decrementAndGet();
+                    logger.info("Added local active bucket {}. {} buckets are remaining.", localBucket.id(), remainingBucketCount);
+                } else {
+                    logger.error("Failed to add local active bucket {}", localBucket);
+                }
             } catch (Exception e) {
-                throw new BucketException("Failed to register master bucket " + bucket, e);
+                logger.error("Failed to register local active bucket to the ZooKeeper.", e);
             }
-
-            availableBucketCount.decrementAndGet();
         }
-    }
-
-    private void registerBucket(Bucket bucket) throws Exception {
-        final String json = new Gson().toJson(bucket.configuration());
-        final String path = ZKPaths.makePath(zooKeeperConfiguration.path(), "bucket", bucket.id());
-        final String createdPath = Databuter.instance().curator()
-                .create()
-                .creatingParentContainersIfNeeded()
-                .withMode(CreateMode.EPHEMERAL)
-                .forPath(path, json.getBytes());
-        logger.debug("Registered bucket {} at {}", bucket.id(), createdPath);
     }
 
     private void onAdded(ChildData data) {
         if (data == null) {
+            logger.warn("Received CHILD_ADDED event with null data.");
             return;
         }
 
         final String json = new String(data.getData());
-        final BucketConfiguration addedBucketConfiguration = new Gson().fromJson(json, BucketConfiguration.class);
+        final BucketConfiguration bucketConfiguration = new Gson().fromJson(json, BucketConfiguration.class);
 
-        if (bucketGroup.has(addedBucketConfiguration.id())) {
-            //로컬 버킷
+        if (bucketGroup.has(bucketConfiguration.id())) {
+            // 이미 버킷 그룹에 버킷이 추가되어있는 경우 무시한다.
             return;
         }
 
-        bucketGroup.add(new RemoteBucket(addedBucketConfiguration));
+        final String nodeId = Databuter.instance().id();
+        if (bucketConfiguration.isActiveBy(nodeId) || bucketConfiguration.isStandbyBy(nodeId)) {
+            // 이 노드의 버킷임에도 불구하고 버킷 그룹에 추가되어있지 않는 경우
+            throw new IllegalStateException("Found not added own bucket " + bucketConfiguration);
+        }
+
+        if (availableBucketCount.get() > 0) {
+            if (StringUtils.isEmpty(bucketConfiguration.standbyNodeId())) {
+                try {
+                    bucketConfiguration.standbyNodeId(nodeId);
+                    final Bucket localBucket = new LocalBucket(bucketConfiguration);
+
+                    final String json2 = new Gson().toJson(localBucket.configuration());
+                    final String path = ZKPaths.makePath(zooKeeperConfiguration.path(), "bucket", localBucket.id());
+                    Databuter.instance().curator()
+                            .setData()
+                            .forPath(path, json2.getBytes());
+
+                    final boolean added = bucketGroup.add(localBucket);
+                    if (added) {
+                        final long remainingBucketCount = availableBucketCount.decrementAndGet();
+                        logger.info("Added local standby bucket {}. {} buckets are remaining.",
+                                localBucket.id(), remainingBucketCount);
+                    } else {
+                        logger.error("Failed to add local standby bucket {}", localBucket);
+                    }
+                } catch (Exception e) {
+                    logger.error("Failed to register local standby bucket to the ZooKeeper.", e);
+                }
+            } else {
+                final Bucket remoteBucket = new RemoteBucket(bucketConfiguration);
+                final boolean added = bucketGroup.add(remoteBucket);
+                if (added) {
+                    logger.info("Added remote bucket {} because it is already high available.", remoteBucket.id());
+                } else {
+                    logger.error("Failed to add remote bucket {}", remoteBucket);
+                }
+            }
+        } else {
+            final Bucket remoteBucket = new RemoteBucket(bucketConfiguration);
+            final boolean added = bucketGroup.add(remoteBucket);
+            if (added) {
+                logger.info("Added remote bucket {} because available bucket count is zero.", remoteBucket.id());
+            } else {
+                logger.error("Failed to add remote bucket {}", remoteBucket);
+            }
+        }
     }
 
-    //TODO(@nono5546):업데이트 구현 후 테스트.
     private void onUpdated(ChildData data) {
         if (data == null) {
+            logger.warn("Received CHILD_UPDATED event with null data.");
             return;
         }
 
         final String json = new String(data.getData());
-        final BucketConfiguration updatedBucketConfiguration = new Gson().fromJson(json, BucketConfiguration.class);
+        final BucketConfiguration bucketConfiguration = new Gson().fromJson(json, BucketConfiguration.class);
 
-        final Bucket bucket = bucketGroup.find(updatedBucketConfiguration.id());
+        final Bucket bucket = bucketGroup.find(bucketConfiguration.id());
         if (bucket == null) {
-            //TODO(@nono5546):에러 발생할 수 있음.
-            bucketGroup.add(new RemoteBucket(updatedBucketConfiguration));
-        } else {
-            bucket.updateConfiguration(updatedBucketConfiguration);
+            throw new IllegalStateException("Found not added bucket " + bucketConfiguration);
         }
+
+        bucket.configuration().update(bucketConfiguration);
+        logger.info("Updated bucket {}.", bucket.id());
     }
 }
