2204433dbaae29b97536530a25f146972a080441
Gihwan Kim <ghkim3221@gmail.com>
2019-07-27 16:22:35 +0900

    엔드포인트 설정 생성

---
+++ src/main/java/databute/databuter/network/EndpointConfiguration.java
@@ -0,0 +1,31 @@
+package databute.databuter.network;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class EndpointConfiguration {
+
+    @JsonProperty("address")
+    private String address;
+
+    @JsonProperty("port")
+    private int port;
+
+    public String address() {
+        return address;
+    }
+
+    public int port() {
+        return port;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("address", address)
+                .add("port", port)
+                .toString();
+    }
+}
