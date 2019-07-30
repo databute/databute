f5127a58ddb03c1ae921bf676b730118cfcdcd81
Gihwan Kim <ghkim3221@gmail.com>
2019-07-31 04:45:18 +0900

    만료 시간 설정 스탠바이 버킷에 적용

--- src/main/java/databute/databuter/entry/expire/ExpireEntryMessageHandler.java
+++ src/main/java/databute/databuter/entry/expire/ExpireEntryMessageHandler.java
@@ -2,11 +2,15 @@ package databute.databuter.entry.expire;
 
 import databute.databuter.Databuter;
 import databute.databuter.bucket.Bucket;
+import databute.databuter.cluster.ClusterCoordinator;
+import databute.databuter.cluster.ClusterNode;
+import databute.databuter.cluster.remote.RemoteClusterNode;
 import databute.databuter.entry.*;
 import databute.databuter.entry.result.fail.EntryOperationFailMessage;
 import databute.databuter.entry.result.success.EntryOperationSuccessMessage;
 import databute.databuter.network.Session;
 import databute.databuter.network.message.AbstractMessageHandler;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +30,6 @@ public class ExpireEntryMessageHandler extends AbstractMessageHandler<Session, E
 
         final String id = expireEntryMessage.id();
         final String key = expireEntryMessage.key();
-        final Instant expirationTimestamp = expireEntryMessage.expirationTimestamp();
 
         try {
             final EntryKey entryKey = new EntryKey(key);
@@ -34,32 +37,76 @@ public class ExpireEntryMessageHandler extends AbstractMessageHandler<Session, E
             if (bucket == null) {
                 // TODO(@ghkim3221): 키에 해당하는 버킷을 찾을 수 없는 경우. 이 경우가 발생할 것인가...?
             } else {
-                bucket.expire(entryKey, expirationTimestamp, new EntryCallback() {
-                    @Override
-                    public void onSuccess(Entry entry) {
-                        session().send(EntryOperationSuccessMessage.entry(id, entry));
-                    }
+                if (StringUtils.equals(bucket.activeNodeId(), Databuter.instance().id())) {
+                    if (StringUtils.isNotEmpty(bucket.standbyNodeId())) {
+                        final ClusterCoordinator clusterCoordinator = Databuter.instance().clusterCoordinator();
+                        final ClusterNode node = clusterCoordinator.find(bucket.standbyNodeId());
+                        if (node instanceof RemoteClusterNode) {
+                            final RemoteClusterNode remoteNode = (RemoteClusterNode) node;
 
-                    @Override
-                    public void onFailure(Exception e) {
-                        if (e instanceof NotFoundException) {
-                            session().send(EntryOperationFailMessage.notFound(id, key));
-                        } else if (e instanceof EmptyEntryKeyException) {
-                            session().send(EntryOperationFailMessage.emptyKey(id, key));
-                        } else if (e instanceof DuplicateEntryKeyException) {
-                            session().send(EntryOperationFailMessage.duplicateKey(id, key));
-                        } else if (e instanceof UnsupportedValueTypeException) {
-                            session().send(EntryOperationFailMessage.unsupportedValueType(id, key));
-                        } else {
-                            logger.error("Unknown error to expire entry {}", key, e);
+                            final EntryMessageDispatcher dispatcher = Databuter.instance().entryMessageDispatcher();
+                            dispatcher.enqueue(expireEntryMessage.id(), new EntryCallback() {
+                                @Override
+                                public void onSuccess(Entry entry) {
+                                    expireEntry(entryKey, bucket, expireEntryMessage);
+                                }
+
+                                @Override
+                                public void onFailure(Exception e) {
+                                    if (e instanceof NotFoundException) {
+                                        session().send(EntryOperationFailMessage.notFound(id, key));
+                                    } else if (e instanceof EmptyEntryKeyException) {
+                                        session().send(EntryOperationFailMessage.emptyKey(id, key));
+                                    } else if (e instanceof DuplicateEntryKeyException) {
+                                        session().send(EntryOperationFailMessage.duplicateKey(id, key));
+                                    } else if (e instanceof UnsupportedValueTypeException) {
+                                        session().send(EntryOperationFailMessage.unsupportedValueType(id, key));
+                                    } else {
+                                        logger.error("Unknown error to remove entry {}", key, e);
+                                    }
+                                }
+                            });
+                            remoteNode.session().send(expireEntryMessage);
                         }
+                    } else {
+                        expireEntry(entryKey, bucket, expireEntryMessage);
                     }
-                });
+                } else {
+                    expireEntry(entryKey, bucket, expireEntryMessage);
+                }
             }
         } catch (EmptyEntryKeyException e) {
             session().send(EntryOperationFailMessage.emptyKey(id, key));
         } catch (UnsupportedValueTypeException e) {
             session().send(EntryOperationFailMessage.unsupportedValueType(id, key));
         }
+    }
+
+    private void expireEntry(EntryKey entryKey, Bucket bucket, ExpireEntryMessage expireEntryMessage) {
+        final String id = expireEntryMessage.id();
+        final String key = expireEntryMessage.key();
+        final Instant expirationTimestamp = expireEntryMessage.expirationTimestamp();
+
+        bucket.expire(entryKey, expirationTimestamp, new EntryCallback() {
+            @Override
+            public void onSuccess(Entry entry) {
+                session().send(EntryOperationSuccessMessage.entry(id, entry));
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+                if (e instanceof NotFoundException) {
+                    session().send(EntryOperationFailMessage.notFound(id, key));
+                } else if (e instanceof EmptyEntryKeyException) {
+                    session().send(EntryOperationFailMessage.emptyKey(id, key));
+                } else if (e instanceof DuplicateEntryKeyException) {
+                    session().send(EntryOperationFailMessage.duplicateKey(id, key));
+                } else if (e instanceof UnsupportedValueTypeException) {
+                    session().send(EntryOperationFailMessage.unsupportedValueType(id, key));
+                } else {
+                    logger.error("Unknown error to expire entry {}", key, e);
+                }
+            }
+        });
     }
 }
