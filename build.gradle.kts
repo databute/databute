687b84dace481d38cb8c9f4d55b35dc3667635e7
Gihwan Kim <ghkim3221@gmail.com>
2021-03-06 18:12:13 +0900

    Gradle Kotlin DSL 적용

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 - 2021 databute
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+plugins {
+    java
+    application
+}
+
+group = "databute"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.9")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.9.9")
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("com.google.guava:guava:28.0-jre")
+    implementation("io.netty:netty-all:4.1.36.Final")
+    implementation("org.apache.commons:commons-lang3:3.9")
+    implementation("org.apache.curator:curator-framework:2.13.0")
+    implementation("org.apache.curator:curator-recipes:2.13.0")
+    implementation("org.slf4j:slf4j-api:1.7.26")
+
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
+
+    testImplementation("junit:junit:4.12")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+application {
+    mainClass.set("databute.databuter.Databuter")
+}
